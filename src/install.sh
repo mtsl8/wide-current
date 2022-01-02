167def5cc6a47ab8d19098ba1e37dd012ec4e03a
mtsl8 <mtsl8@widemage.net>
2022-01-01 23:57:37 -0500

    Create install.sh

---
+++ src/install.sh
@@ -0,0 +1,27 @@
+#!/WIP
+
+# add checks to make sure everything is set up right
+
+. parse_yaml.sh
+
+eval $(parse_yaml conf.yml "conf_")
+
+# disk operations
+
+partition () { }
+
+# encryption() { }
+
+filesystem () { }
+
+# core system installation
+
+mount () { }
+
+strap () { }
+
+chroot () { }
+
+setup () { }
+
+# system will reboot
